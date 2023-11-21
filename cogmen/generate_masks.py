92b191389a771f2d96ef9a758fa33f417f2cd899
naston <nathan@planetpaull.com>
2023-11-20 22:10:16 -0600

    removed print

--- cogmen/generate_masks.py
+++ cogmen/generate_masks.py
@@ -20,27 +20,20 @@ def generate_training_mask(cfg):
 
     if p<p_mask_audio_full:
         # fully mask audio
-        print('Fully Masking Audio')
         audio_mask = np.zeros(audio_input_length)
         video_mask = np.ones(video_input_length)
     elif p<(p_mask_video_full + p_mask_audio_full):
         # fully mask video
-        print('Fully Masking Video')
         audio_mask = np.ones(audio_input_length)
         video_mask = np.zeros(video_input_length)
     else:
         # random masking
-        print('Performing Random Masking')
 
         audio_mask = np.ones(audio_input_length)
         video_mask = np.ones(video_input_length)
 
         audio_mask_start_ids = np.where(np.random.random(size=audio_input_length,) < p_mask_audio_rand)
         video_mask_start_ids = np.where(np.random.random(size=video_input_length) < p_mask_video_rand)
-        
-        print()
-        print('Audio Mask Init:',audio_mask_start_ids)
-        print('Video Mask Init:',video_mask_start_ids)
 
         audio_mask_id_offsets = np.arange(audio_mask_length)
         video_mask_id_offsets = np.arange(video_mask_length)
@@ -50,10 +43,6 @@ def generate_training_mask(cfg):
 
         video_mask_ids = np.unique(np.add.outer(video_mask_start_ids, video_mask_id_offsets).flatten())
         video_mask_ids = video_mask_ids[video_mask_ids<video_input_length]
-
-        print()
-        print('Audio Mask IDs:',audio_mask_ids)
-        print('Video Mask IDs:',video_mask_ids)
 
         audio_mask[audio_mask_ids] = 0
         video_mask[video_mask_ids] = 0
